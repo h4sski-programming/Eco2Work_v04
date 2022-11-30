081e393bdf6b5fab31ecec5d3abf6f9eb0e27342
h4sski <h4sski.programming+git@gmail.com>
2022-12-01 00:50:29 +0100

    Update. Activity table for all users is working.

--- eco2work/e2w_app/views.py
+++ eco2work/e2w_app/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 
-from datetime import date
+from datetime import date, timedelta
 
 from .models import Activity
 from .forms import RegisterForm, LoginForm
@@ -12,46 +12,25 @@ from .forms import RegisterForm, LoginForm
 
 @login_required(login_url='login_view')
 def index(request):
+    today = date.today()
+    month_first_day = today.replace(day=1)
+    month_last_day = today.replace(month=today.month + 1, day=1) - timedelta(days=1)
+
+    days_list = [n for n in range(month_first_day.day, month_last_day.day + 1)]
+
     activity_all = Activity.objects.all()
     users_all = User.objects.all()
-    days_list = [n for n in range(1, 31)]
-    month_sum = 0
 
-    today = date.today()
-
-    for a in activity_all:
-        month_sum += a.distance
-
-    def fill_activities_to_display():
-        #   {'user':[[a1, a2, a3, a4], sum]
-        output = {}
-        for u in users_all:
-            output[u.username] = [[], 0]
-
+    month_sum = {}
+    for u in users_all:
+        user_sum = 0
         for a in activity_all:
-            output[a.user.username][0].append(a)
-
-        for u in output:
-            u_sum = 0
-            for act in output[u][0]:
-                u_sum += act.distance
-                print(act)
-            output[u][1] = u_sum
-
-        return output
-
-    # fill_activities_to_display()
-    activities_to_display = fill_activities_to_display()
-
-    test_dic = [
-            ['user1', [1, 2, 34, 5, 67, 6], 5],
-            ['user2', [5, 3, 8, 6, 3, 23, 3], 50],
-        ]
+            if a.user == u:
+                user_sum += a.distance
+        month_sum[u] = user_sum
 
     context = {
         'activity_all': activity_all,
-        'activities_to_display': activities_to_display,
-        'test_dic': test_dic,
         'users_all': users_all,
         'days_list': days_list,
         'today': today,
